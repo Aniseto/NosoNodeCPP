e7c2066d440dfe0085eeda8ea9c2106c14884931
Aniseto <alka_parra@hotmail.com>
2024-02-02 11:50:35 +0100

    First server client setup

--- NodeCPP.cpp
+++ NodeCPP.cpp
@@ -2,19 +2,66 @@
 //
 
 #include <iostream>
+#include <boost/asio.hpp>
+#include <thread>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace boost::asio;
+using ip::tcp;
+
+class P2PServer {
+public:
+    P2PServer(io_service& ioService, unsigned short port)
+        : acceptor_(ioService, tcp::endpoint(tcp::v4(), port)),
+        socket_(ioService) {
+        startAccept();
+    }
+
+private:
+    void startAccept() {
+        acceptor_.async_accept(socket_,
+            [this](const boost::system::error_code& ec) {
+                if (!ec) {
+                    std::cout << "Client Connected\n";
+
+                    // Create a Thread to manage the new connection
+                    std::thread([this]() {
+                        handleConnection();
+                        }).detach();
+                }
+
+                startAccept();
+            });
+    }
+
+    void handleConnection() {
+        // Manage connection from client
+
+        // Example: Send a message to client
+        std::string message = "Hello, client!";
+        boost::asio::write(socket_, boost::asio::buffer(message));
+
+        // Close the connection
+        socket_.close();
+        std::cout << "Connexion closed\n";
+    }
+
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+};
+
+int main() {
+    try {
+        boost::asio::io_service ioService;
+        unsigned short port = 12345;  // Default Port, can be changed using argument
+        P2PServer server(ioService, port);
+
+        std::cout << "Server listening on port " << port << std::endl;
+
+        ioService.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
